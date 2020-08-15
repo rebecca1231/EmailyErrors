49a315bee791f90df4e52765604a38562402d6f0
Rebecca <rebeccagetsanemail@gmail.com>
2020-08-15 14:46:36 +0900

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+//the model has to be imported before passport or errors
+//so we define the model before passport tries to access and use it
+require("./models/User");
+require("./services/passport");
+
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) =>
+    console.log(`connected to ${db.connections[0].name} collection`)
+  );
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there", bye: "later"});
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey],
+  })
+);
 
-const PORT = process.env.PORT || 5000
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
