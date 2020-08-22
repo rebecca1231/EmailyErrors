951b8e2598a5608d0bfe3a2823b07b1eef9f2c40
Rebecca <rebeccagetsanemail@gmail.com>
2020-08-22 16:39:08 +0900

    emails are working

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,30 +1,37 @@
-import _ from 'lodash'
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import formFields from './formFields' 
+import { withRouter } from 'react-router-dom'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
-    const reviewFields = _.map(formFields, ({name, label}) => {
-        return (
-            <div key={name} >
-                <label>{label}</label>
-                <div>
-                    {formValues[name]}
-                </div>
-            </div>
-        )
-    })
+import formFields from "./formFields";
+import * as actions from "../../actions";
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <h5>Please confirm your entries.</h5>
       {reviewFields}
-      <button onClick={onCancel} className="yellow darken-3 white-text btn-flat">
+      <button
+        onClick={onCancel}
+        className="yellow darken-3 white-text btn-flat"
+      >
         Back
       </button>
-      <button className='green btn-flat right white-text'>
-          Send Survey
-          <i className='material-icons right'>email</i>
+      <button
+        onClick={() => submitSurvey(formValues, history)}
+        className="green btn-flat right white-text"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -33,4 +40,4 @@ const mapStateToProps = (state) => {
   return { formValues: state.form.surveyForm.values };
 };
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   { label: "Survey Title", name: "title" },
   { label: "Subject", name: "subject" },
   { label: "Body", name: "body" },
-  { label: "List of Recipients", name: "emails" },
+  { label: "List of Recipients", name: "recipients" },
 ];
